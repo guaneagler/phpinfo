485127e29b21d151118dfb1311587b839edb0090
Eagle <eagleg@ciandt.com>
2020-06-22 15:54:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+if( ini_get('safe_mode') ){
+   // safe mode is on
+  echo "safe";
+}else{
+   // it's not
+  echo "not safe";
+}
 set_time_limit(0);
 echo "start";
-sleep(400);
+//sleep(400);
 echo "end";
+echo ini_get("max_execution_time");
 //phpinfo();
