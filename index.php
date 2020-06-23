8038f6b5a44e2ac7542ccfa2265a6f4ab4ebe051
eagle <eagleg@ciandt.com>
2020-06-23 09:22:36 +0800

    Update

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-phpinfo();
-return;
 if( ini_get('safe_mode') ){
    // safe mode is on
   echo "safe";
@@ -8,9 +6,7 @@ if( ini_get('safe_mode') ){
    // it's not
   echo "not safe";
 }
-set_time_limit(550);
 echo "start";
 sleep(400);
 echo "end";
-echo ini_get("max_execution_time");
 //phpinfo();
